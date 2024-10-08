354e6930b2c4cab751997ab87e122585cb320998
Rafanambinantsoa <akutagawakarim@gmail.com>
2024-10-08 10:57:42 +0300

    [Mise en place des Attributs Users Done]

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'nom' => fake()->name(),
+            'contact' => fake()->phoneNumber(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
