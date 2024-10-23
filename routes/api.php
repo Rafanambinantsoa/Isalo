af8b4cf00618b2f94f370d555d22f3f53d8bbf12
Rafanambinantsoa <akutagawakarim@gmail.com>
2024-10-23 10:23:37 +0300

    Module fournisseur terminé : contrôleur et méthode de paiement implémentés

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\CongerController;
+use App\Http\Controllers\FournisseurController;
+use App\Http\Controllers\MethodePaimentController;
 use App\Http\Controllers\PosteController;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\TypeCongerController;
@@ -16,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
 
 // Authentication
 Route::post('/register', [RegisteredUserController::class, 'store']);
@@ -34,18 +35,22 @@ Route::resource('postes', PosteController::class)->middleware('auth:sanctum');
 Route::get('/postes/employee/{id}', [PosteController::class, 'getListEmployeeByPoste']);
 
 //Related route for conger
-Route::resource('congers' , CongerController::class);
+Route::resource('congers', CongerController::class);
 Route::resource('type_congers', TypeCongerController::class);
 Route::patch('/congers/{id}/accept', [CongerController::class, 'acceptConger']);
 
+//Related route for FOurnisseur
+Route::resource('fournisseurs', FournisseurController::class);
+
+//Related route for Methode paiment  fournisseur
+Route::resource('paiments', MethodePaimentController::class);
 //test
 Route::get('/test-email', function () {
     Mail::raw('Test email body', function ($message) {
         $message->to('tsukasashishiosama@gmail.com')
-                ->subject('Test Email');
+            ->subject('Test Email');
     });
-    
+
     return 'Email sent!';
 });
-Route::get('/test' , [TestController::class , 'index']);
-
+Route::get('/test', [TestController::class, 'index']);
