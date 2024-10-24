e8f9d226b0535267caf77d0b408f3bb7c1631186
Rafanambinantsoa <akutagawakarim@gmail.com>
2024-10-24 11:47:30 +0300

    Mise en place de la table 'Produits' avec ses attributs, factory, CRUD et endpoints

---
+++ app/Http/Controllers/ProduitController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ProduitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $all = Produit::all();
+        if ($all->isEmpty()) {
+            return response()->json(['message' => 'No products found (vide) '], 404);
+        }
+        return response()->json(Produit::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nom' => ['required', 'string'],
+            'categorie_id' => ['required', 'integer', 'exists:categories,id'],
+            'quantite' => ['required', 'integer'],
+            'prix' => ['required', 'integer'],
+            'fournisseur_id' => ['required', 'integer', 'exists:fournisseurs,id'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $produit = Produit::create($request->all());
+
+        return response()->json($produit, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $produit = Produit::find($id);
+
+        if (!$produit) {
+            return response()->json([
+                'message' => 'Produit not found',
+            ]);
+        }
+        return response()->json($produit);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Produit $produit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Produit $produit)
+    {
+        $validator = Validator::make($request->all(), [
+            'nom' => ['required', 'string'],
+            'categorie_id' => ['required', 'integer', 'exists:categories,id'],
+            'quantite' => ['required', 'integer'],
+            'prix' => ['required', 'integer'],
+            'fournisseur_id' => ['required', 'integer', 'exists:fournisseurs,id'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $produit->update($request->all());
+        $produit->save();
+
+        return response()->json($produit, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $produit = Produit::find($id);
+        if (!$produit) {
+            return response()->json(['message' => 'Produit not found'], 404);
+        }
+        $produit->delete();
+        return response()->json(['message' => 'Produit deleted successfully'], 200);
+    }
+}
---
+++ app/Models/Produit.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produit extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProduitFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'nom',
+        'categorie_id',
+        'quantite',
+        'prix',
+        'fournisseur_id',
+    ];
+}
